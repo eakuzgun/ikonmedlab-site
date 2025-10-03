9684f4e02d1d1ba4e8cceb3dd62624aa8522ce32
Enes Ahmet Kuzgun <eakuzgun@gmail.com>
2025-10-04 02:28:21 +0300

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
+  output: 'export',
+  assetPrefix: isProd ? '/ikonmedlab-site/' : '', // 🔹 repo adını buraya yaz
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
